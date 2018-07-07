b7ec1fb97c0e18fa1f9bc0ccaee7349ab9b8d791
johannesvollmer <johannes.boenki@t-online.de>
2018-07-07 16:08:47 +0200

    Made the project a library by renaming main.rs to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,18 @@ pub mod id;
 pub use map::*;
 pub use id::*;
 
-fn main() {
+
+#[cfg(test)]
+fn example() {
     let mut words = IdMap::new();
 
     let id_hello = words.insert("hello");
     let _id_world = words.insert("world");
 
     println!("{:?} -> {:?}", id_hello, words.get(id_hello));
+    assert_eq!(words.get(id_hello), Some("hello"));
 
     words.mark_unused(id_hello);
+    assert_eq!(words.get(id_hello), None);
+
 }
