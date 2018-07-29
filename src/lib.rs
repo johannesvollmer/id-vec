be928c88ae33532fede75fc2346d0abd4fb6d6c3
johannesvollmer <johannes.boenki@t-online.de>
2018-07-29 15:23:53 +0200

    Added extended example of a `graph`

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,65 @@ mod examples {
     use super::*;
 
     #[test]
+    fn nodes() {
+
+        #[derive(Debug)]
+        struct Node {
+            parent: Option<Id<Node>>,
+            name: String,
+        }
+
+
+
+        let mut nodes = IdVec::new();
+
+        let root: Id<Node> = nodes.insert(Node {
+            parent: None,
+            name: String::from("Root"),
+        });
+
+        let orphan = nodes.insert(Node {
+            parent: None,
+            name: String::from("Orphan")
+        });
+
+        let child = nodes.insert(Node {
+            parent: Some(root),
+            name: String::from("Child")
+        });
+
+        println!("{:?}", nodes);
+
+
+
+
+        // run a basic garbage collector,
+        // keeping all objects which have a parent, and the root itself
+        {
+            nodes.retain(|node_id, node|{
+                node_id == root || node.parent.is_some()
+            });
+
+            // attention! child_a was removed and thus the id becomes invalid!
+            assert!(!nodes.contains_id(orphan));
+            assert!(nodes.contains_id(root));
+            assert!(nodes.contains_id(child));
+        }
+
+
+
+        // create a cyclic graph
+        {
+            // to dereference the node, we index into the id-vec
+            let root_mut = &mut nodes[root];
+
+            // creating a cycle is possible
+            root_mut.parent = Some(child);
+        }
+    }
+
+
+    #[test]
     fn example1() {
         let map = id_vec!("hello", "world");
         debug_assert!(map.contains_element(&"hello"));
